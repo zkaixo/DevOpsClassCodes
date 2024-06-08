e2f4f56c18d6d25c3e4954f4a201829264e0f53a
zkaixo <jarim.olaechea@gmail.com>
2024-06-08 13:22:44 -0400

    Update authentication.tf

--- authentication.tf
+++ authentication.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAXUWA2HMCHJFYM2WG"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
